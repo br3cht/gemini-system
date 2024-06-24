e2ad315c917be5463d79a16ad0242135bf558d79
br3cht <ant_175@hotmail.com>
2024-06-24 13:38:04 -0300

    feat: added new input in ChatInput
    feat: added new use case to pro vision

--- app/DTO/Gemini/ChatInput.php
+++ app/DTO/Gemini/ChatInput.php
@@ -2,10 +2,13 @@
 
 namespace App\DTO\Gemini;
 
+use Illuminate\Http\UploadedFile;
+
 class ChatInput
 {
     public function __construct(
-        public readonly string $text,
+        public readonly string|null $text,
+        public readonly UploadedFile|null $image = null,
     ) {
     }
 }
